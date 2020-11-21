b05efcc74f58bb5d0b615cf004cad6b42384cdd4
Mohammed Seidu <seyid24itc@gmail.com>
2020-11-21 23:15:01 +0000

    Added day one files to day1 branch

---
+++ day1/main.py
@@ -0,0 +1,4 @@
+print("Hello World")
+print("Hello " + input("What is your name:") + "!")
+
+print(len(input("Please provide your name to count chars:")))
---
+++ day1/teamname.py
@@ -0,0 +1,7 @@
+# print welcome message
+print("welcome to the team name generator !")
+# ask user for where they want to establish their team
+location = input("Enter geographic location of your team: ")
+prefered_postfix = input("Enter prefered postfix: ")
+
+print(location + " " + prefered_postfix + " could be the name of your team")
